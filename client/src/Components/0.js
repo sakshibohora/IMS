a36ba27d7fb07ff1790a15a7a3287d63dc0d2d2c
Sakshi <sakshibohora>
2019-03-11 10:38:14 +0530

    new middle component

---
+++ client/src/Components/0.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react'
+import { Route } from "react-router-dom";
+import AuthService from './AuthService';
+import withAuth from './withAuth';
+import PropTypes from 'prop-types';
+import ListUser from './listuser';
+import AddUser from './Adduser'
+
+let formData = {
+  username: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  contactNo: '',
+  role: '',
+  status: '',
+  email: '',
+}
+class a0 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      formData: { ...formData },
+      flag: false,
+      id: ''
+    }
+    this.Auth = new AuthService();
+    this.updateData = this.updateData.bind(this);
+    this.updateState = this.updateState.bind(this);
+    this.updateformData = this.updateformData.bind(this);
+    this.editData = this.editData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  updateData(data) {
+    this.setState({ data })
+  }
+
+  updateformData() {
+    this.setState({ formData: { ...formData } })
+  }
+
+  updateState() {
+    this.setState({ flag: false, formData: { ...formData }, id: '' })
+  }
+  editData(element) {
+    // console.log(element.username)
+    const formData1 = {
+      username: element.username,
+      password: element.password,
+      firstName: element.firstName,
+      lastName: element.lastName,
+      email: element.email,
+      contactNo: element.contactNo,
+      role: element.role,
+      status: element.status,
+    }
+    this.setState({ formData: formData1, flag: true, id: element.id })
+
+  }
+
+  handleChange(e, target, field) {
+    e.preventDefault();
+    const temp = { ...this.state[target] };
+    temp[field] = e.target.value;
+    this.setState({ [target]: temp })
+  }
+  render() {
+    const { match } = this.props
+    return (
+      <>
+            <Route path={`${match.url}/listuser`} render={(props) =>
+              <ListUser
+                {...props}
+                handleSearch={this.handleSearch}
+                {...this.state}
+                updateData={this.updateData}
+                editData={this.editData}
+              />
+            }
+            />
+
+            <Route path={`${match.url}/adduser`} render={(props) =>
+              <AddUser
+                {...props}
+                {...this.state}
+                updateState={this.updateState}
+                updateformData={this.updateformData}
+                handleChange={this.handleChange}
+              />
+            }
+            />
+      </>
+    );
+  }
+}
+
+a0.defaultProps = {
+  match: {},
+};
+a0.propTypes = {
+  match: PropTypes.object,
+};
+export default withAuth(a0);
