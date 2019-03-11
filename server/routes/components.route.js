88ee08dd0b3930fa23d0fc27a7f7ebf5d3a0cb1c
Sakshi <sakshibohora>
2019-03-11 10:37:33 +0530

    routing modified

--- server/routes/components.route.js
+++ server/routes/components.route.js
@@ -1,9 +1,9 @@
 const components = require('../controllers/components.controller');
 
 const routes = (app) => {
-  app.get('/api/components/', components.getAllComponents);
+  app.get('/api/components/list', components.getAllComponents);
   app.post('/api/components', components.createNewComponents);
-  app.put('/api/components/:id', components.updateComponents);
+  app.put('/api/components/edit/:id', components.updateComponents);
   app.delete('/api/components/:id', components.deleteComponents);
 
   app.post('/api/components/getComponentName', components.getComponentName);
