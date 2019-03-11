3929105a8ca7f0d5d5700ab424dfda6db56a1aa7
Sakshi <sakshibohora>
2019-03-11 10:26:21 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './Components/Login'
 import Home from './Components/Home'
 import Footer from './Components/Footer';
 import './App.css';
 import Admin from './Components/Admin';
-import ViewComponentStatus from './Components/ViewComponentStatus';
-
 class App extends Component {
   render() {
     return (
       <>
         <Router>
-          <>
-            {/* <Route exact path='/' component={Main} /> */}
+          <Switch>
             <Route exact path='/' component={Login} />
-            <Route path='/home' component={Home} />
-            <Route path='/admin' component={Admin} />
-            <Route exact path='/home/viewComponentStatus' component={ViewComponentStatus} />
-            {/* <Redirect from='/admin' to='/home' /> 
-            <Redirect to='/admin' from='/home' />  */}
-          </>
+            <Route path='/home' render={props => <Home {...props} />} />
+            <Route path='/admin' render={props => <Admin {...props} />} />
+          </Switch>
         </Router>
         <Footer />
       </>
