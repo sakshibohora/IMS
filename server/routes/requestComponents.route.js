6b66b2ccfd1c106ea29af687659035890ff203a8
Sakshi <sakshibohora>
2019-03-15 16:09:40 +0530

    routing changed

--- server/routes/requestComponents.route.js
+++ server/routes/requestComponents.route.js
@@ -1,13 +1,13 @@
 const reqcomponents = require('../controllers/requestComponent.controller');
 
 const routes = (app) => {
-  app.get('/api/requestComponents/', reqcomponents.getAllRequestedComponents);
+  app.get('/api/requestComponents/list', reqcomponents.getAllRequestedComponents);
   app.post('/api/requestComponents', reqcomponents.createNewRequestComponents);
-  app.put('/api/requestComponents/:id', reqcomponents.updateRequestedComponent);
+  app.put('/api/requestComponents/edit/:id', reqcomponents.updateRequestedComponent);
   app.delete('/api/requestComponents/:id', reqcomponents.deleteRequestedComponents);
 
   app.post('/api/requestComponents/requestComponentByUser', reqcomponents.getRequestedComponentByUser);
-
+  app.get('/api/requestcomponents/find/:id',reqcomponents.getComponent)
 };
 
 module.exports = { routes };
