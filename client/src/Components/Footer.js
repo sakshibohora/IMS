37fc06034d009654202ad575a8d5d940242871b8
Sakshi <sakshibohora>
2019-03-15 16:00:02 +0530

    modification

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 export default class Footer extends Component {
-    render() {
-        return (
-            <div style={{ width: '100%', position:"fixed",bottom: '0', fontSize: '20px', textAlign: "center", color: "white"}} >
-                <span>Copyright © Bacancy Technology 2019</span>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={{ width: '100%', position: "fixed", bottom: '0', fontSize: '20px', textAlign: "center"}} >
+        <span></span>
+        <footer style={{color:"black"}}>Copyright © Bacancy Technology 2019</footer>
+      </div>
+    
+    );
+  }
+  
 }
