f561259106a574aa0814955ef0d2565300a43a85
Sakshi <sakshibohora>
2019-03-15 16:09:00 +0530

    routing changed

--- server/routes/categories.route.js
+++ server/routes/categories.route.js
@@ -1,11 +1,10 @@
 const categories = require('../controllers/categories.controller');
 
 const routes = (app) => {
-  app.get('/api/categories/list', categories.getAllCategories);
+  app.post('/api/categories/list', categories.getAllCategories);
   app.post('/api/categories', categories.createNewCategories);
-  app.put('/api/categories/:id', categories.updateCategories);
-  app.delete('/api/categories/:id', categories.deleteCategories);
-
+  app.put('/api/categories/edit/:id', categories.updateCategories);
+  app.delete('/api/categories/delete/:id', categories.deleteCategories);
   app.get('/api/categories/getCategoryId', categories.getCategoryId)
 };
 
