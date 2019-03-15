93c7530e781992b744feea4c3ef84948e0bd40b4
Sakshi <sakshibohora>
2019-03-15 16:01:09 +0530

    changed

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import AuthService from './AuthService';
-import withAuth from './withAuth';
+import Sidebar from './Sidebar';
+import SidebarAdmin from './sidebarAdmin'
+import { Link } from 'react-router-dom';
 const Auth = new AuthService();
-
 class Navbar extends Component {
   constructor(props) {
     super(props);
@@ -14,16 +15,15 @@ class Navbar extends Component {
     Auth.logout()
     this.props.history.replace('/');
   }
-
   render() {
+    let role=this.props.user.role
     return (
       <>
         <nav className="navbar navbar-expand navbar-dark bg-dark static-top">
-          <a className="navbar-brand mr-1" href="index.html"><img src={require("../images/bacancy-technology2.png")} height="40px" width="200px" /></a>
-          <button className="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-            <i className="fas fa-bars"></i>
-          </button>
-
+          <Link className="navbar-brand mr-1" to="/">
+            <img src={require("../images/bacancy-technology2.png")} height="40px" width="200px" />
+          </Link>
+          {this.props.user.role ? <SidebarAdmin /> : <Sidebar />}
           <form className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
             <div className="input-group">
               <input type="text" className="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"></input>
@@ -35,37 +35,13 @@ class Navbar extends Component {
             </div>
           </form>
           <ul className="navbar-nav ml-auto ml-md-0">
-            <li className="nav-item dropdown no-arrow mx-1">
-              <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i className="fas fa-bell fa-fw"></i>
-                {/* <span className="badge badge-danger">9+</span> */}
-              </a>
-              <div className="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item dropdown no-arrow mx-1">
-              <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i className="fas fa-envelope fa-fw"></i>
-                {/* <span className="badge badge-danger">7</span> */}
-              </a>
-              <div className="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
             <li className="nav-item dropdown no-arrow">
               <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {this.props.user.username}<i className="fas fa-user-circle fa-fw"></i>
               </a>
               <div className="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                <a className="dropdown-item" href="#">Settings</a>
-                <a className="dropdown-item" href="#">Activity Log</a>
+                {/* <a className="dropdown-item" href="#">Settings</a>
+                <a className="dropdown-item" href="#">Activity Log</a> */}
                 <div className="dropdown-divider"></div>
                 {/* <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a> */}
                 <input type='button' value='Logout' className="btn btn-danger" onClick={this.handleLogout.bind(this)} />
@@ -78,4 +54,4 @@ class Navbar extends Component {
   }
 }
 
-export default withAuth(Navbar);
+export default Navbar;
