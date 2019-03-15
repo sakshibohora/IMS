0f7bd6a9a737f2e215bd0b5c391eaad34c58d1be
Sakshi <sakshibohora>
2019-03-15 16:09:27 +0530

    routing changed

--- server/routes/incidentUpdates.route.js
+++ server/routes/incidentUpdates.route.js
@@ -1,10 +1,10 @@
 const incidentUpdates = require('../controllers/incidentUpdates.controller');
 
 const routes = (app) => {
-  app.get('/api/incidentUpdates/', incidentUpdates.getAllIncidentUpdates);
+  app.get('/api/incidentUpdates/list', incidentUpdates.getAllIncidentUpdates);
   app.post('/api/incidentUpdates', incidentUpdates.createNewIncidentupdates);
   app.put('/api/incidentUpdates/:id', incidentUpdates.updateIncidentUpdates);
-  app.delete('/api/incidentUpdates/:id', incidentUpdates.deleteIncidentUpdates);
+  app.delete('/api/incidentUpdates/delete:id', incidentUpdates.deleteIncidentUpdates);
 };
 
 module.exports = { routes };
