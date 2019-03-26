d2f995407c86d3e804c8f9265278b345d684ada8
Sakshi <sakshibohora>
2019-03-26 19:04:06 +0530

    deleted

--- server/routes/assigedComponent.route.js
+++
@@ -1,8 +0,0 @@
-const assignedComponent = require('../controllers/assignedcomponent.controller');
-
-const routes = (app) => {
-  app.get('/api/assignedComponent/list', assignedComponent.getAllAssignedComponent);
-  app.post('/api/assignedcomponent', assignedComponent.assignComponent);
-};
-
-module.exports = { routes };
