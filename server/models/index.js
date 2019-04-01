42a05e10e950aa697e6619647368cda2eb70d1e2
Sakshi <sakshibohora>
2019-04-01 11:23:26 +0530

    association added

--- server/models/index.js
+++ server/models/index.js
@@ -42,27 +42,29 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-db.Users=require('../models/users.js')(sequelize,Sequelize)
-db.Categories=require('../models/categories.js')(sequelize,Sequelize)
-db.RequestComponents=require('../models/requestComponents.js')(sequelize,Sequelize)
-db.Incidents=require('../models/incidents.js')(sequelize,Sequelize)
-db.Components = require('../models/components.js')(sequelize, Sequelize); 
+db.Users = require('../models/users.js')(sequelize, Sequelize)
+db.Categories = require('../models/categories.js')(sequelize, Sequelize)
+db.RequestComponents = require('../models/requestComponents.js')(sequelize, Sequelize)
+db.Incidents = require('../models/incidents.js')(sequelize, Sequelize)
+db.Components = require('../models/components.js')(sequelize, Sequelize);
 db.AssignedComponents = require('../models/assignedcomponents.js')(sequelize, Sequelize);
+db.IncidentUpdates = require('../models/incidentUpdates.js')(sequelize, Sequelize);
 
 
+db.RequestComponents.belongsTo(db.Categories, { foreignKey: 'categoryId' })
+db.RequestComponents.belongsTo(db.Users, { foreignKey: 'userId' })
+db.Incidents.belongsTo(db.Users, { foreignKey: 'incidentBy' })
+db.Incidents.belongsTo(db.Users, { as:'ResolvedBy',foreignKey: 'resolvedBy' })
 
-db.RequestComponents.belongsTo(db.Categories,{foreignKey:'categoryId'})
-db.RequestComponents.belongsTo(db.Users,{foreignKey:'userId'})
-db.Incidents.belongsTo(db.Users,{foreignKey:'incidentBy'})
-
-
+db.IncidentUpdates.belongsTo(db.Users, { as: 'UserDetails', foreignKey: 'updateBy' })
+db.IncidentUpdates.belongsTo(db.Incidents, { as: 'IncidentDetails', foreignKey: 'incidentId' })
 
 
 //Relationships
-db.Components.belongsTo(db.Categories, {foreignKey: 'categoryId'});
-db.AssignedComponents.belongsTo(db.Categories, {foreignKey: 'categoryId'});
-db.AssignedComponents.belongsTo(db.Components, {foreignKey: 'componentId'});
-db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedBy',foreignKey: 'assignedBy'});
-db.AssignedComponents.belongsTo(db.Users, {as: 'AssignedTo', foreignKey: 'userId'});
+db.Components.belongsTo(db.Categories, { foreignKey: 'categoryId' });
+db.AssignedComponents.belongsTo(db.Categories, { foreignKey: 'categoryId' });
+db.AssignedComponents.belongsTo(db.Components, { foreignKey: 'componentId' });
+db.AssignedComponents.belongsTo(db.Users, { as: 'AssignedBy', foreignKey: 'assignedBy' });
+db.AssignedComponents.belongsTo(db.Users, { as: 'AssignedTo', foreignKey: 'userId' });
 
 module.exports = db;
